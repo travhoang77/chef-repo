72808541bd561c1dabeb9abe0a35bbcd6e6410c1
Travis Hoang <travhoang@chef-workstation.travis-domain.net>
2014-05-08 03:31:40 -0700

    Commit message

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'travhoang'
+client_key               '/home/travhoang/chef-repo/.chef/travhoang.pem'
+validation_client_name   'chef-validator'
+validation_key           '/home/travhoang/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://10.0.0.10:443'
+syntax_check_cache_path  '/home/travhoang/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/home/travhoang/chef-repo/cookbooks' ]
