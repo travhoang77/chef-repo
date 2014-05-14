4d65b203d189cc6c69c682ba034ba48248eeb8f5
Travis Hoang <travhoang@chef-workstation.travis-domain.net>
2014-05-13 22:09:02 -0700

    the book env

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,3 +7,4 @@ validation_key           '/home/travhoang/chef-repo/.chef/chef-validator.pem'
 chef_server_url          'https://10.0.0.10:443'
 syntax_check_cache_path  '/home/travhoang/chef-repo/.chef/syntax_check_cache'
 cookbook_path [ '/home/travhoang/chef-repo/cookbooks' ]
+knife[:editor]='/usr/bin/vim'
--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -5,4 +5,7 @@
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
+include_recipe "chef-client"
+include_recipe "apt"
+include_recipe "ntp"
+
